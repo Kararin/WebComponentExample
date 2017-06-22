76e0a61ee942134ea2eeee3ed525323c4d6ff89e
Kararin <charms_devil@mail.ru>
2017-06-23 00:36:28 +0300

    smiles drawing changed to canvas

--- cmp/smile.js
+++ cmp/smile.js
@@ -9,7 +9,8 @@ const cb = () => {
         var clone = document.importNode(template, true);
         shadowRoot.appendChild(clone);
 
-        this.el = shadowRoot.querySelector('.smile');
+        this.el = shadowRoot.querySelector('#smile');
+        this.ctx = this.el.getContext('2d');
 
         this.setSmile(JSON.parse(this.getAttribute('smile')));
     };
@@ -21,7 +22,96 @@ const cb = () => {
     };
 
     MyElementProto.setSmile = function(val) {
-        this.el.innerHTML = val ? '&#9786;' : '&#9785;';
+        this.el.innerHTML = val ? this.drawHappy() : this.drawSad();
+    };
+
+    MyElementProto.drawHappy = function() {
+        this.clean();
+        this.drawFace();
+        this.drawHappyEyes();
+        this.drawSmile();
+    };
+
+    MyElementProto.drawHappyEyes = function() {
+        let width = this.el.width,
+            height = this.el.height,
+            topPadding = height / 1.8,
+            leftPadding = width / 3;
+
+        this.ctx.beginPath();
+        this.ctx.moveTo(width / 2 - 10, 20);
+        this.ctx.lineTo(width / 2 - 25, 30);
+        this.ctx.moveTo(width / 2 + 10, 20);
+        this.ctx.lineTo(width / 2 + 25, 30);
+        this.ctx.stroke();
+        this.ctx.closePath();
+    };
+
+    MyElementProto.drawSmile = function() {
+        let width = this.el.width,
+            height = this.el.height;
+
+        this.ctx.beginPath();
+        this.ctx.arc(width / 2, height / 2, width / 3, 0, Math.PI, false);
+        this.ctx.stroke();
+        this.ctx.closePath();
+    };
+
+    MyElementProto.help = function() {
+        let width = this.el.width,
+            height = this.el.height;
+
+        this.ctx.beginPath();
+        this.ctx.moveTo(0, height / 2);
+        this.ctx.lineTo(width, height / 2);
+        this.ctx.moveTo(width / 2, 0);
+        this.ctx.lineTo(width / 2, height);
+        this.ctx.stroke();
+        this.ctx.closePath();
+    };
+
+    MyElementProto.drawSad = function() {
+        this.clean();
+        this.drawFace();
+        this.drawSadEyes();
+        this.drawSadSmile();
+    };
+
+    MyElementProto.drawSadEyes = function() {
+        let width = this.el.width,
+            height = this.el.height;
+
+        this.ctx.beginPath();
+        this.ctx.arc(width / 2 - 13, 25, 3, 0, Math.PI * 2);
+        this.ctx.arc(width / 2 + 13, 25, 3, 0, Math.PI * 2);
+        this.ctx.fill();
+        this.ctx.closePath();
+    };
+
+    MyElementProto.drawSadSmile = function() {
+        let width = this.el.width,
+            height = this.el.height;
+
+        this.ctx.beginPath();
+        this.ctx.moveTo(width / 2 - 20, height / 2 + 15);
+        this.ctx.lineTo(width / 2 + 20, height / 2 + 15);
+        this.ctx.stroke();
+        this.ctx.closePath();
+    };
+
+    MyElementProto.drawFace = function() {
+        let width = this.el.width,
+            height = this.el.height,
+            r = width / 2;
+
+        this.ctx.beginPath();
+        this.ctx.arc(width / 2, height / 2, r, 0, 2 * Math.PI, true);
+        this.ctx.stroke();
+        this.ctx.closePath();
+    };
+
+    MyElementProto.clean = function() {
+        this.ctx.clearRect(0, 0, this.el.height, this.el.width);
     };
 
     window.MyElement = document.registerElement('my-smiley', {
